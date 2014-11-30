9fefa2f5e5c7fb23e67b019cd226d5f6df2d6b15
compedit <troybetz@gmail.com>
2014-11-29 22:12:44 -0600

    cooler examples

--- example/example.js
+++ example/example.js
@@ -3,16 +3,19 @@
  */
 
 var fillParent = require('../');
-
-var container = document.querySelector('.container');
+var container = document.querySelector('.content');
 
 container.addEventListener('click', function(event) {
   var el = event.target;
-  
+
+  if (!event.target.classList.contains('childElement')) {
+    return;
+  }
+
   if (el.classList.contains('is-selected')) {
     el.setAttribute('style', '');
   } else {
-    fillParent(el, container, {x: 100, y: 100});
+    fillParent(el, el.parentElement, {x: 40, y: 40});
   }
 
   el.classList.toggle('is-selected');
